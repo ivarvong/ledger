1edf8d8380de153268e762060c4859b31cb4eab7
Ivar Vong <ivar@ivarvong.com>
2016-04-28 19:50:11 -0400

    add ecto, postgrez

--- mix.exs
+++ mix.exs
@@ -10,24 +10,13 @@ defmodule Ledger.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {Ledger, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:postgrex, ">= 0.0.0"},
+     {:ecto, "~> 2.0.0-beta"}]
   end
 end
